997db5c3a5e432991b9a3f027f7473fe7ad39b33
Jacob Pelrah <pelrahjsp@gmail.com>
2024-08-21 21:45:40 -0400

    add images

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-function createHeader(){
+function createHeader() {
     const header = document.createElement('header')
 
     // create and style the nav
@@ -23,16 +23,16 @@ function createHeader(){
     navImg.style.height = '2em'
     navImg.style.marginRight = '1em'
     // bark when image is clicked
-    navImg.addEventListener('click', bark)
-    navImg.style.cursor='pointer'
+    navImg.addEventListener('click', (e) => bark(e.target))
+    navImg.style.cursor = 'pointer'
 
-    
+
     // create navbar text
     const navBarText = document.createTextNode('Zelda the Pomeranian')
-    const navItems = [navImg, navBarText ]
+    const navItems = [navImg, navBarText]
 
     // construct the nav
-    navItems.forEach(node=>navbarBrand.appendChild(node))
+    navItems.forEach(node => navbarBrand.appendChild(node))
     containerDiv.appendChild(navbarBrand)
     navBar.appendChild(containerDiv)
     header.appendChild(navBar)
@@ -40,21 +40,55 @@ function createHeader(){
     return header
 }
 
+// create media card fragment
+function createMediaCardFragment(imgPath, video = false) {
+    const fragment = new DocumentFragment()
 
-function bark(){
-    const randomBark = Math.floor(Math.random() * 4 + 1);
-    console.log(randomBark)
-    const audio = new Audio(`src/assets/audio/bark_${randomBark}.wav`)
-    audio.play()
+    // create media card
+    const card = document.createElement('div')
+    card.classList.add('card')
+
+
+    const cardImg = document.createElement('img')
+    cardImg.classList.add('card-img-top')
+    cardImg.src = imgPath
+ 
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+
+    card.appendChild(cardImg)
+    card.appendChild(cardBody)
+
+    return fragment.appendChild(card)
 }
 
-
-
+function bark(target) {
+    target.classList.add('bark')
+    target.style.pointerEvents = 'none'
+    const randomBark = Math.floor(Math.random() * 4 + 1);
+    const audio = new Audio(`src/assets/audio/bark_${randomBark}.wav`)
+    audio.addEventListener('ended', () => {
+        target.classList.remove('bark')
+        target.style.pointerEvents = 'auto'
+    })
+    audio.play()
+}
 
 
 // program setup
 const bootstrapCDN = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>`
 document.head.insertAdjacentHTML('beforeend', bootstrapCDN)
 
-const body = document.body
-body.appendChild(createHeader())
+// insert header
+const body = document.body.insertAdjacentElement('afterbegin', createHeader())
+const container = document.querySelector('.container')
+container.classList.add('mt-5')
+
+
+// insert media card fragments
+
+
+const cardGroup = document.getElementById('card-group')
+for (let i = 1; i < 24; i++) {
+    cardGroup.appendChild(createMediaCardFragment(`src/assets/images/z_${i}.jpg`))
+}
